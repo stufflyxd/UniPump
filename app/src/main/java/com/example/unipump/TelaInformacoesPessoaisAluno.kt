292ad97426941f49269a42e71536945254d05c36
Luluzin <peedox@hotmail.com>
2025-04-20 13:09:29 -0300

    Tela Pricipal e Tela Treino

--- app/src/main/java/com/example/unipump/TelaInformacoesPessoaisAluno.kt
+++ app/src/main/java/com/example/unipump/TelaInformacoesPessoaisAluno.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class TelaAluno : AppCompatActivity() {
+class TelaInformacoesPessoaisAluno : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela_aluno)
+        setContentView(R.layout.activity_tela_informacoes_pessoais_aluno)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
